c9ce9a8550bd048eaad4288836898142527531d1
sudonomen <trystinduffy@gmail.com>
2023-04-07 00:49:24 -0500

    2rd commit

--- tryst/src/pages/index.js
+++ tryst/src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Hello;
             <code className={styles.code}>src/pages/index.js</code>
           </p>
           <div>
